dd0f683b07adbded1bd1cab84b730a591d0699ee
Fernando Castellanos <26000111@galileo.edu>
2026-03-09 22:54:35 -0600

    agregamos codigo e imagen

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+echo "<h1>Hello World</h1>";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Actividad 3 PHP</title>
+</head>
+<body>
+
+    <p>Hola mundo, mi nombre es Fernando Castellanos.</p>
+
+    <!-- Agregar una imagen -->
+    <img src="proyecto_php.png" alt="Mi imagen con AI" width="600">
+
+</body>
+</html>
